4659950a4889e2ee458482ae68b534e454778f5e
Alifio Rasyid <official.alifio@gmail.com>
2019-11-25 12:15:07 +0700

    Added Basic UI on MemberPane

--- sample/Controller.java
+++ sample/Controller.java
@@ -11,7 +11,6 @@ import javafx.scene.text.Font;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Filter;
 
 public class Controller implements Initializable {
 
@@ -26,11 +25,19 @@ public class Controller implements Initializable {
     @FXML private AnchorPane OrderPane;
     @FXML private AnchorPane MemberPane;
 
+    // Order Pane Members
     @FXML private Label NewOrderLabel;
     @FXML private Label DeleteOrderLabel;
     @FXML private Label OverviewLabel;
     @FXML private ComboBox FilterComboBox;
     @FXML private ComboBox DateComboBox;
+
+    // Member Pane Members
+    @FXML private Label NewMemberLabel;
+    @FXML private Label DeleteMemberLabel;
+    @FXML private Label OverviewLabel2;
+    @FXML private ComboBox FilterComboBox2;
+    @FXML private ComboBox DateComboBox2;
 
     @Override
     public void initialize(URL url, ResourceBundle rb){
@@ -42,6 +49,14 @@ public class Controller implements Initializable {
         FilterComboBox.getItems().addAll("All", "Pending", "Confirmed");
         DateComboBox.setPromptText("Date: Today");
         DateComboBox.getItems().addAll("All", "Today", "Yesterday");
+
+        // Initialize Member Pane
+        NewMemberLabel.setFont(Font.loadFont("file:src/fonts/cocoregular.ttf", 14));
+        DeleteMemberLabel.setFont(Font.loadFont("file:src/fonts/cocoregular.ttf", 14));
+        OverviewLabel2.setFont(Font.loadFont("file:src/fonts/cocolight.ttf", 14));
+        FilterComboBox2.setPromptText("Status: All");
+        FilterComboBox2.getItems().addAll("All", "Business", "Individuals");
+
         // By Default, Order Label is Clicked
         OrderLabelClicked();
     }
