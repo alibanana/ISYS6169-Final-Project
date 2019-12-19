7550b168e30a1202ff40baa29a667a2ece7563c5
nicholasm185 <43334128+nicholasm185@users.noreply.github.com>
2019-12-19 13:52:38 +0700

    Libs Included

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -8,7 +8,7 @@ public class Database {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost/db_databasesystem";
     static final String USER = "root";
-    static final String PASS = "2201798295Binus";
+    static final String PASS = "";
     static Connection conn;
     static Statement stmt;
     static ResultSet rs;
